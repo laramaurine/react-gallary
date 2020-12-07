8fcf98382b3903fcd50a43f748d48e9bb90d5909
Malik Elate <Malik.e1955@gmail.com>
2020-12-06 20:29:38 -0600

    grammar fix

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,6 +1,6 @@
 const galleryItems = [
     { id: 1, path: './images/protest.png', title: 'Car Ablaze', description: 'Car set on fire during protest, Minneapolis Lake Street Target.', year: 2020, likes: 0 },
-    { id: 2, path: './images/basketball.png', title: 'Gym Day', description: 'A photo accidentally taken during while training alone.', year: 2019, likes: 0 },
+    { id: 2, path: './images/basketball.png', title: 'Gym Day', description: 'A photo accidentally taken while training alone.', year: 2019, likes: 0 },
     { id: 3, path: './images/dog.png', title: 'Sunny', description: 'My dog Sunny suited up for a muddy day.', year: 2017, likes: 0 },
     { id: 4, path: './images/cameroon.png', title: 'Le Pays', description: 'Playing basketball in Cameroon, greatest winter break in history.', year: 2016, likes: 0 },
     { id: 5, path: './images/friends.jpeg', title: 'Friends', description: 'A legendary parking lot tailgate with the best friends you can ask for.', year: 2018, likes: 0 },
